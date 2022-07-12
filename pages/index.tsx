ef15c7f8000f031b840cf7884bc4784349a79ee8
Hector <halvarez@taylormadetech.net>
2022-07-12 17:53:55 -0500

    added title and icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Lock from "../public/cyber_padlock.svg";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-slate-300 h-full">
       <Head>
         <title>Taylored Secrets</title>
         <meta name="description" content="One Time Taylored Secrets" />
@@ -12,16 +13,15 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1>
-          Welcome to Taylored Secrets!
-        </h1>
+        <div className="mx-auto flex max-w-5xl justify-between border-l-2 border-r-2 border-slate-600 px-8 pt-6 items-center">
+          <h1 className="text-7xl font-bold">Taylored Secrets</h1>
+          <Image src={Lock} alt="connected lock" width={128} height={128} />
+        </div>
       </main>
 
-      <footer>
-
-      </footer>
+      <footer></footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
